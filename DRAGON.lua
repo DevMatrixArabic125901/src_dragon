b868c632bfa7135a4e18a38ef987d4ddcd423bf5
JasTn ExTra <119659334+qithoniq@users.noreply.github.com>
2023-09-26 21:21:27 +0300

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -3913,7 +3913,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if Devahmed(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/qithoniq/Files_Dragon/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3951,7 +3951,7 @@ t = " ℘︙ الملف » "..file.."\n ℘︙ تم تعطيل ملف \n"
 else
 t = " ℘︙ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/qithoniq/Files_Dragon/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3971,7 +3971,7 @@ t = " ℘︙ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ℘︙ الملف » "..file.."\n ℘︙ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/SRC-DRAGON/Files_Dragon/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/qithoniq/Files_Dragon/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
